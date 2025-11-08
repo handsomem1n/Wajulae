ced8c350ef16c8610735ca13145b2afccfa95684
k25903796-crypto <k25903796@gmail.com>
2025-11-08 23:21:07 +0900

    asdf
    daf

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'  // <- 확장자 없이
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
